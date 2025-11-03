0079246e1e2f9d6a75f297a262053dee26fcb3d6
yonal5 <yonalabasingha@gmail.com>
2025-11-03 10:06:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,18 +15,30 @@ app.use(cors())
 
 app.use(express.json())
 
-app.use((req, res, next) => {
-  let token = req.header("Authorization");
-  if (token) {
-    token = token.replace("Bearer ", "");
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (!err && decoded) {
-        req.user = decoded;
-      }
-    });
-  }
-  next();
-});
+app.use(
+    (req,res,next)=>{
+
+        let token = req.header("Authorization")
+
+        if(token != null){
+            token = token.replace("Bearer ","")
+            jwt.verify(token, process.env.JWT_SECRET,
+                (err, decoded)=>{
+                    if(decoded == null){
+                        res.json({
+                            message: "Invalid token please login again"
+                        })
+                        return
+                    }else{
+                        req.user = decoded
+                    }
+                }
+            )
+
+        }
+        next()
+    }
+)
 
 const connectionString = process.env.MONGO_URI
 
