253c57c0c5b57e3cc439e085ff806f4f16aa046f
yonal5 <yonalabasingha@gmail.com>
2025-12-26 15:42:13 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,43 +1,15 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
-    {
-        email : {
-            type : String,
-            required : true,
-            unique : true
-        },
-        firstName : {
-            type : String,
-            required : true
-        },
-        lastName : {
-            type : String,
-            required : true
-        },
-        password : {
-            type : String,
-            required : true
-        },
-        role : {
-            type : String,
-            required : true,
-            default : "user"
-        },
-        isBlock: {
-            type : Boolean,
-            default : false
-        },
-        isEmailVerified:{
-            type : Boolean,
-            default : false
-        },
-        image : {
-            type : String,
-            default : "/user.png"
-        }
-    }
-)
+const userSchema = new mongoose.Schema({
+	email: { type: String, required: true, unique: true },
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	password: { type: String, required: true },
+	role: { type: String, default: "user" },
+	isBlock: { type: Boolean, default: false },
+	isEmailVerified: { type: Boolean, default: false },
+	image: { type: String, default: "/user.png" }
+});
 
-const User = mongoose.model("User",userSchema)
-export default User
+const User = mongoose.model("User", userSchema);
+export default User;
