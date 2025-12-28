313e319b08d5f7e1f45de67ad3ff01e8995ae09f
yonal5 <yonalabasingha@gmail.com>
2025-12-28 17:51:49 +0530

    Add files via upload

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,7 +7,7 @@ const userRouter = express.Router();
 userRouter.post("/",createUser)
 userRouter.post("/login",loginUser)
 userRouter.post("/google-login",googleLogin)
-userRouter.get("/all-users", getAllUsers)
+userRouter.get("/all-users", authMiddleware, getAllUsers);
 userRouter.put("/block/:email",blockOrUnblockUser)
 userRouter.get("/send-otp/:email",sendOTP)
 userRouter.post("/change-password/",changePasswordViaOTP)
