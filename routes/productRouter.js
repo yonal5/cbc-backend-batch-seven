287093c4dae12fa5236d3ac82d4bdb78fc47caae
yonal5 <yonalabasingha@gmail.com>
2026-01-02 18:14:26 +0530

    Update productRouter.js

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,16 +1,21 @@
 
 import express from 'express';
 import { createProduct, deleteProduct, getProductId, getProducts, updateProduct } from '../controllers/productController.js';
+import { authMiddleware } from '../controllers/authMiddleware.js';
 
 const productRouter = express.Router();
 
 productRouter.get("/",getProducts)
 productRouter.post("/", createProduct)
-productRouter.delete("/:productID", deleteProduct);
-productRouter.put("/:productID",updateProduct)
-productRouter.get("/:productID", getProductId)
+import { authMiddleware } from "../controllers/authMiddleware.js";
+
+productRouter.post("/", authMiddleware, createProduct);
+productRouter.put("/:productID", authMiddleware, updateProduct);
+productRouter.delete("/:productID", authMiddleware, deleteProduct);
+
 
 
 
 
 export default productRouter;
+a
