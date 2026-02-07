70604ea04efe7c762af4790fe74b0d4fbd058282
yonal5 <yonalabasingha@gmail.com>
2026-02-07 13:31:10 +0530

    Update chatModel.js

--- models/chatModel.js
+++ models/chatModel.js
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
 
-const chatSchema = new mongoose.Schema(
-  {
-    guestId: { type: String, required: true },
-    customerName: { type: String, required: true },
-    sender: { type: String, enum: ["customer", "admin"], required: true },
-    type: { type: String, enum: ["text", "image"], default: "text" },
-    message: { type: String },
-    imageUrl: { type: String },
-    isRead: { type: Boolean, default: false },
-  },
-  { timestamps: true }
-);
+const chatSchema = new mongoose.Schema({
+  guestId: String,
+  customerName: String,
+  sender: { type: String, enum: ["customer", "admin"] },
+  message: String,
+  imageUrl: String,
+  type: { type: String, enum: ["text", "image"] },
+  isRead: Boolean
+}, { timestamps: true });
+
 
 export default mongoose.model("Chat", chatSchema);
