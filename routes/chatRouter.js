7052b6e5018d4a8144e308d28041995f5715dd8c
yonal5 <yonalabasingha@gmail.com>
2026-02-07 12:21:58 +0530

    Update chatRouter.js

--- routes/chatRouter.js
+++ routes/chatRouter.js
@@ -1,21 +1,18 @@
 import express from "express";
 import {
-sendMessage,
-getMessages,
-listCustomers,
-adminGetMessages,
-adminSend,
+  sendMessage,
+  getMessages,
+  listCustomers,
+  adminGetMessages,
+  adminSend,
 } from "../controllers/chatController.js";
 
 const router = express.Router();
 
-// CUSTOMER
-router.post("/chat", sendMessage);
-router.get("/chat", getMessages);
-
-// ADMIN
-router.get("/customers", listCustomers);
-router.get("/admin", adminGetMessages);
-router.post("/admin/send", adminSend);
+router.post("/", sendMessage);          // Customer send
+router.get("/", getMessages);           // Customer get messages
+router.get("/customers", listCustomers);// Admin get customers
+router.get("/admin", adminGetMessages); // Admin get messages
+router.post("/admin/send", adminSend);  // Admin send
 
 export default router;
