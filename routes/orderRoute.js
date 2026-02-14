b241838a691bb166e4392f5844fc46c600f11af5
yonal5 <yonalabasingha@gmail.com>
2026-02-14 22:15:14 +0530

    Update orderRoute.js

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -5,16 +5,10 @@ import {
   updateOrderStatus,
 } from "../controllers/orderController.js";
 
-
 const router = express.Router();
 
-/* CREATE ORDER */
 router.post("/", createOrder);
-
-/* GET ORDERS */
 router.get("/", getOrders);
-
-/* UPDATE ORDER STATUS */
 router.put("/:orderID", updateOrderStatus);
 
 export default router;
