e2f72041895ddfd01f9adb6533c29d56ecd1767a
yonal5 <yonalabasingha@gmail.com>
2026-02-14 21:57:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import chatRouter from "./routes/chatRouter.js"
 import adminRouter from "./routes/adminRouter.js";
-
+import orderRoute from "./src/routes/orderRoute.js";
 
 dotenv.config();
 
@@ -62,7 +62,7 @@ app.use("/api/products", productRouter)
 app.use(express.json());
 app.use(cors());
 app.use("/api/chat", chatRouter);
-
+app.use("/api/orders", orderRoute);
 app.use("/api/admin", adminRouter);
 app.use("/uploads", express.static("uploads"));
 app.use("/api/chat", chatRouter);
