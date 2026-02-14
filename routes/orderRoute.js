bb11d7f66b909ddd488840c5f2ebe81616804a6b
yonal5 <yonalabasingha@gmail.com>
2026-02-14 20:50:38 +0530

    Update orderRoute.js

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -15,4 +15,20 @@ router.get("/", getOrders);
 
 router.put("/status/:orderID", updateOrderStatus);
 
+router.get("/admin", authMiddleware, async (req, res) => {
+  try {
+
+    if (req.user.role !== "admin") {
+      return res.status(403).json({ message: "Not authorized" });
+    }
+
+    const orders = await Order.find().sort({ createdAt: -1 });
+
+    res.json(orders);
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 export default router;
