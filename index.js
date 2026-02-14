0fde0c62372a9086363354f756d4ef7b86723ae2
yonal5 <yonalabasingha@gmail.com>
2026-02-14 22:15:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,75 @@
 import express from "express";
-import {
-  createOrder,
-  getOrders,
-  updateOrderStatus,
-} from "../controllers/orderController.js";
+import mongoose from "mongoose";
+import userRouter from "./routes/userRouter.js";
+import jwt from "jsonwebtoken";
+import productRouter from "./routes/productRouter.js";
+import cors from "cors";
+import dotenv from "dotenv";
+import chatRouter from "./routes/chatRouter.js"
+import adminRouter from "./routes/adminRouter.js";
+import orderRoute from "./routes/orderRoute.js";
 
-const router = express.Router();
+dotenv.config();
 
-router.post("/", createOrder);
-router.get("/", getOrders);
-router.put("/:orderID", updateOrderStatus);
+const app = express()
+app.use(cors())
 
-export default router;
+app.use(express.json())
+
+app.use(
+    (req,res,next)=>{
+
+        let token = req.header("Authorization")
+
+        if(token != null){
+            token = token.replace("Bearer ","")
+            jwt.verify(token, process.env.JWT_SECRET,
+                (err, decoded)=>{
+                    if(decoded == null){
+                        res.json({
+                            message: "Invalid token please login again"
+                        })
+                        return
+                    }else{
+                        req.user = decoded
+                    }
+                }
+            )
+
+        }
+        next()
+    }
+)
+
+const connectionString = process.env.MONGO_URI
+
+
+mongoose.connect(connectionString).then(
+    ()=>{
+        console.log("Database connected Successfully")
+    }
+).catch(
+    ()=>{
+        console.log("Database connection failed")
+    }
+)
+
+
+
+
+app.use("/api/users",userRouter)
+app.use("/api/products", productRouter)
+app.use(express.json());
+app.use(cors());
+app.use("/api/chat", chatRouter);
+app.use("/api/orders", orderRoute);
+app.use("/api/admin", adminRouter);
+app.use("/uploads", express.static("uploads"));
+app.use("/api/chat", chatRouter);
+
+
+app.listen(5000, 
+    ()=>{
+        console.log("Server is running on port 5000")
+    }
+)
