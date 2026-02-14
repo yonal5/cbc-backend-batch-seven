b541ca3484d35a2c1da326fe07e29abaceb2582a
yonal5 <yonalabasingha@gmail.com>
2026-02-14 22:24:43 +0530

    Update orderRoute.js

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -5,17 +5,12 @@ import {
   updateOrderStatus,
 } from "../controllers/orderController.js";
 
-import { authMiddleware } from "../middleware/auth.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-/* CREATE ORDER */
 router.post("/", authMiddleware, createOrder);
-
-/* GET ORDERS */
 router.get("/", authMiddleware, getOrders);
-
-/* UPDATE ORDER STATUS */
 router.put("/:orderID", authMiddleware, updateOrderStatus);
 
 export default router;
