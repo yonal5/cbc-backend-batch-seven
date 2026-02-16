a0abd535840092afbdd14b1eba19aee8a3a24eee
yonal5 <yonalabasingha@gmail.com>
2026-02-16 21:24:55 +0530

    Update order.js

--- models/order.js
+++ models/order.js
@@ -1,54 +1,60 @@
 import mongoose from "mongoose";
 
-const orderSchema = new mongoose.Schema(
-  {
-    orderID: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-
-    items: [
-      {
-        productID: String,
-        quantity: Number,
-        name: String,
-        price: Number,
-        image: String,
+const orderSchema =
+  new mongoose.Schema(
+    {
+      orderID: {
+        type: String,
+        required: true,
+        unique: true,
       },
-    ],
 
-    customerName: {
-      type: String,
-      required: true,
-    },
+      items: [
+        {
+          productID: String,
+          name: String,
+          price: Number,
+          quantity: Number,
+          image: String,
+        },
+      ],
 
-    email: {
-      type: String,
-      required: true,
-    },
+      customerName: {
+        type: String,
+        required: true,
+      },
 
-    phone: {
-      type: String,
-      required: true,
-    },
+      email: {
+        type: String,
+        required: true,
+      },
 
-    address: {
-      type: String,
-      required: true,
-    },
+      phone: {
+        type: String,
+        required: true,
+      },
 
-    total: {
-      type: Number,
-      required: true,
-    },
+      address: {
+        type: String,
+        required: true,
+      },
 
-    status: {
-      type: String,
-      default: "Pending",
+      total: {
+        type: Number,
+        required: true,
+      },
+
+      status: {
+        type: String,
+        default: "Pending",
+      },
     },
-  },
-  { timestamps: true }
+    {
+      timestamps: true,
+    }
+  );
+
+export default mongoose.model(
+  "Order",
+  orderSchema
 );
-
-export default mongoose.model("Order", orderSchema);
