a387ede09380946dd8460dad308a8a84c5b63da9
yonal5 <yonalabasingha@gmail.com>
2026-02-17 17:08:24 +0530

    Update order.js

--- models/order.js
+++ models/order.js
@@ -1,60 +1,56 @@
 import mongoose from "mongoose";
 
-const orderSchema =
-  new mongoose.Schema(
-    {
-      orderID: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-
-      items: [
-        {
-          productID: String,
-          name: String,
-          price: Number,
-          quantity: Number,
-          image: String,
-        },
-      ],
-
-      customerName: {
-        type: String,
-        required: true,
-      },
-
-      email: {
-        type: String,
-        required: true,
-      },
-
-      phone: {
-        type: String,
-        required: true,
-      },
-
-      address: {
-        type: String,
-        required: true,
-      },
-
-      total: {
-        type: Number,
-        required: true,
-      },
-
-      status: {
-        type: String,
-        default: "Pending",
-      },
+const orderSchema = new mongoose.Schema(
+  {
+    orderID: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    {
-      timestamps: true,
-    }
-  );
 
-export default mongoose.model(
-  "Order",
-  orderSchema
+    items: [
+      {
+        productID: String,
+        name: String,
+        price: Number,
+        quantity: Number,
+        image: String,
+      },
+    ],
+
+    customerName: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+    },
+
+    address: {
+      type: String,
+      required: true,
+    },
+
+    total: {
+      type: Number,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      default: "Pending",
+    },
+  },
+  {
+    timestamps: true, // ✅ This creates createdAt & updatedAt
+  }
 );
+
+export default mongoose.model("Order", orderSchema);
